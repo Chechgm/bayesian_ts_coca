4c3ec8b8bc0786cb26fd8fe4516d4e82519a1a0a
Emiliano <orangebacked@pop-os.localdomain>
2020-04-15 22:52:16 -0500

    upload with results and changed evaluation with args

--- train.py
+++ train.py
@@ -1,40 +1,79 @@
-##################################################################################
-# This script contains the neccesary function in order to run and evaluate a single
-# model.
-#
-# Ideally we would like to have:
-#   1. Some form of result saving scheme
-#   2. Some form of outter loop to run this script on all the models (maybe in another script)
-##################################################################################
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 21:32:47 2020
 
-import argparse
+@author: orangebacked
+"""
+
 import pystan
-import pandas as pd
-import numpy as np
+from numpy import genfromtxt
+import time
+import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("data_path",  help="Path where the data lies")
-parser.add_argument("model_path", help="Path where the model definition lies")
-parser.add_argument("L",          help="Initial value from which we estimate the models")
-args = parser.parse_args()
+parser.add_argument('--data_dir', default='./data/formated_data/agregated.csv',
+                    help="Directory containing the dataset")
+parser.add_argument('--model_dir', default='./models/ar_1.stan',
+                    help="Directory containing model def")
 
-# Load the data
-data = data_loading(args.path)
+parser.add_argument('--results_folder', default='./results/',
+                    help="Optional, name of the where the previous fit is")
 
-# Load stan model
-with open (args.model_path, "r") as model_definition:
-    model = model_defintion.read()
+parser.add_argument('--fit', default=None,
+                    help="Optional, name of the where the previous fit is")  
 
-# Create Stan model object (compile Stan model)
-sm = pystan.StanModel(model_code=model)
 
-# Define the data for the model
-model_data = {
-    "N_row": data.shape[0],
-    "N_col": data.shape[1],
-    "y": data,
-    "L": args.L
-}
 
-# Model evaluation
-loo, ks, re_i = psis_lfo_cv(model, model_data, args.L)
+
+
+
+if __name__ == '__main__':
+
+    # Load the parameters from json file
+    args = parser.parse_args()
+    data_dir = args.data_dir 
+    modeldir = args.model_dir
+    results_folder = args.results_folder
+    
+    with open(modeldir, 'r') as f:
+        model_definition = f.read()
+        
+    
+
+    
+#    sm = pystan.StanModel(model_code=model_definition)
+
+
+    aggregated = genfromtxt(data_dir, delimiter=',')
+
+    data = {
+        "N_row": aggregated.shape[0],
+        "N_col": aggregated.shape[1], 
+        "y": aggregated, 
+        "L": 14
+    }
+    
+    sm = pystan.StanModel(model_code=model_definition)
+
+    t0 = time.time()
+    
+    fit = sm.sampling(data=data, iter=2000, warmup=1000, chains=5, algorithm="NUTS", seed=42, verbose=True, 
+                      control={"adapt_delta":0.9, "max_treedepth":15}) # Recommendations from the results
+    
+    
+    t1 = time.time()
+    
+    total = t1-t0
+    
+    timefile = results_folder + 'time.txt'
+    
+    output_model = results_folder + 'output.txt'
+# TODO rename it with the name of the model
+    with open(timefile, 'w') as f:
+        f.write('The run time was: {}'.format(str(total)))
+        
+        
+    with open('output_model.txt', 'w') as f:
+        f.write(str(fit))
+    
